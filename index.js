87a88c776270cff812437f12c0821e8c1701ee49
Clay Anderson <supersonicclay@gmail.com>
2015-11-12 23:52:57 -0700

    WIP

--- index.js
+++ index.js
@@ -15,26 +15,36 @@
     options.icon.failure  = options.icon.failure  || logSymbols.error;
     options.icon.success  = options.icon.success  || logSymbols.success;
     options.icon.ignore   = options.icon.ignore   || logSymbols.info;
+    options.divider       = options.divider       || '\n================================================================================\n';
     options.color         = options.color         || {};
     options.color.failure = options.color.failure || 'red';
     options.color.success = options.color.success || 'green';
     options.color.ignore  = options.color.ignore  || 'blue';
+    options.color.divider = options.color.divider || 'gray';
 
     if (hasColors) {
       this.USE_COLORS      = true;
       options.icon.failure = colorInto(options.color.failure, options.icon.failure);
       options.icon.success = colorInto(options.color.success, options.icon.success);
       options.icon.ignore  = colorInto(options.color.ignore,  options.icon.ignore);
+      options.divider      = colorInto(options.color.divider, options.divider);
     } else {
       this.USE_COLORS      = false;
       options.icon.failure = noColor(options.icon.failure);
       options.icon.success = noColor(options.icon.success);
       options.icon.ignore  = noColor(options.icon.ignore);
+      options.divider = noColor(options.divider);
     }
 
     this.onRunStart = function() {
       this._dotsCount = 0;
     };
+    
+    this.onRunComplete = function() {
+      if (options.divider) {
+      //  this._writeCharacter(options.divider);
+      }
+    }
 
     this.specSuccess = function() {
       this._writeCharacter(options.icon.success);
